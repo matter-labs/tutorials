f5a805545d038d22d21686c20f220fe10825188f
Antonio <tonioufa@gmail.com>
2022-10-10 12:34:02 +0100

    new docker image 1.2.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,23 @@
-require("@matterlabs/hardhat-zksync-deploy");
-require("@matterlabs/hardhat-zksync-solc");
+require('@matterlabs/hardhat-zksync-deploy')
+require('@matterlabs/hardhat-zksync-solc')
 
 module.exports = {
   zksolc: {
-    version: "1.1.5",
-    compilerSource: "docker",
+    version: '1.2.0',
+    compilerSource: 'docker',
     settings: {
       optimizer: {
         enabled: true,
       },
       experimental: {
-        dockerImage: "matterlabs/zksolc",
-        tag: "v1.1.5"
+        dockerImage: 'matterlabs/zksolc',
+        tag: 'v1.2.0',
       },
     },
   },
   zkSyncDeploy: {
-    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
-    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
+    ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
   },
   networks: {
     hardhat: {
@@ -25,6 +25,6 @@ module.exports = {
     },
   },
   solidity: {
-    version: "0.8.16",
+    version: '0.8.16',
   },
-};
+}
