02bba40b114f1eb696b5a3095d9a7586fce5d560
Antonio <tonioufa@gmail.com>
2023-01-11 16:04:09 +0000

    updates deps and compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,21 @@
-require('@matterlabs/hardhat-zksync-deploy');
-require('@matterlabs/hardhat-zksync-solc');
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
 
 module.exports = {
   zksolc: {
-    version: '1.2.1',
-    compilerSource: 'binary',
+    version: "1.2.2",
+    compilerSource: "binary",
     settings: {},
   },
-  defaultNetwork: 'zkSyncTestnet',
-
   networks: {
     zkSyncTestnet: {
-      url: 'https://zksync2-testnet.zksync.dev',
-      ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
     },
   },
+  defaultNetwork: "zkSyncTestnet",
   solidity: {
-    version: '0.8.16',
+    version: "0.8.16",
   },
 };
