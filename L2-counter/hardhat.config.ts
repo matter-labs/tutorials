aa90bb002c142284de6ace840ec47dcd853130a4
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-06-21 09:06:49 -0500

    chore: update deps

--- L2-counter/hardhat.config.ts
+++ L2-counter/hardhat.config.ts
@@ -2,14 +2,11 @@ require('@matterlabs/hardhat-zksync-deploy');
 require('@matterlabs/hardhat-zksync-solc');
 
 module.exports = {
-  zksolc: {
-    version: '1.2.1',
-    compilerSource: 'binary',
-    settings: {},
-  },
   defaultNetwork: 'zkSyncTestnet',
-
   networks: {
+     hardhat: {
+      zksync: true,
+    },
     zkSyncTestnet: {
       url: 'https://zksync2-testnet.zksync.dev',
       ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
@@ -17,6 +14,6 @@ module.exports = {
     },
   },
   solidity: {
-    version: '0.8.16',
+    version: '0.8.19',
   },
 };
