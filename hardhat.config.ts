945924aad65441c04d4b9f81256b9b9c65a0a5ae
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-07-25 15:28:34 -0500

    feat: unit tests for contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,21 +3,38 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
+// dynamically changes endpoints for local tests
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        // Verification endpoint for Goerli
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
 const config: HardhatUserConfig = {
   zksolc: {
     version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
     settings: {},
   },
-  defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: "zkSyncLocalTestnet",
   networks: {
     hardhat: {
-      zksync: true,
+      zksync: false,
     },
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    zkSyncLocalTestnet : {
+      url: "http://localhost:3050",
+      ethNetwork: "http://localhost:8545",
       zksync: true,
-    },
+    }
   },
   solidity: {
     version: "0.8.17",
---
+++ test/MyERC20.test.ts
@@ -0,0 +1,52 @@
+import { expect } from "chai";
+import { Wallet, Provider, Contract } from "zksync-web3";
+import * as hre from "hardhat";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import * as ethers from "ethers";
+import "@matterlabs/hardhat-zksync-chai-matchers";
+
+import { deployContract, fundAccount } from "./utils";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+const GAS_LIMIT = 6000000;
+
+describe("MyERC20", function () {
+    let provider: Provider;
+    let wallet: Wallet;
+    let deployer: Deployer;
+    let userWallet: Wallet;
+    let erc20: Contract;
+
+    before(async function () {
+        provider = new Provider("http://localhost:3050");
+        wallet = new Wallet(PRIVATE_KEY, provider);
+        deployer = new Deployer(hre, wallet);
+
+        userWallet = Wallet.createRandom();
+        userWallet = new Wallet(userWallet.privateKey, provider);
+
+        erc20 = await deployContract(deployer, "MyERC20", ["TestToken", "TTK", 18]);
+    });
+
+    async function getToken(wallet: Wallet) {
+        const artifact = hre.artifacts.readArtifactSync("MyERC20");
+        return new ethers.Contract(erc20.address, artifact.abi, wallet);
+    }
+
+    it("should mint tokens to the specified address", async function () {
+        const amount = ethers.BigNumber.from(100);
+        const tx = await erc20.connect(wallet).mint(userWallet.address, amount);
+        await tx.wait();
+        const balance = await erc20.balanceOf(userWallet.address);
+        console.log(`Balance: ${balance}`)
+        expect(balance).to.be.eql(amount);
+    });
+
+    it("should have correct decimals", async function () {
+        const decimals = await erc20.decimals();
+        expect(decimals).to.be.eql(18);
+    });
+});
