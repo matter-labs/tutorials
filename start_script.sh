a752120db91e381e16063b9a7c7b315803ab1828
abilevych <anton@matterlabs.dev>
2023-08-25 19:27:50 +0300

    chore: updated the CI config

--- start_script.sh
+++ start_script.sh
@@ -6,6 +6,6 @@ ARG2_DEFAULT="testnet"
 PORT="${1:-$ARG1_DEFAULT}"
 NETWORK="${2:-$ARG2_DEFAULT}"
 
-socat TCP-LISTEN:"$PORT",fork,reuseaddr TCP:127.0.0.1:3051 &
+socat TCP-LISTEN:"$PORT",fork,reuseaddr TCP:127.0.0.1:3051 </dev/null &
 era_test_node --port 3051 fork "$NETWORK" &
 wait
