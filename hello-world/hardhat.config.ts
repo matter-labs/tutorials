1001b193b006fc3bad06e1c456a8196ecc66af66
abilevych <anton@matterlabs.dev>
2023-08-28 16:18:40 +0300

    chore: optimize configurator hardhat

--- hello-world/hardhat.config.ts
+++ hello-world/hardhat.config.ts
@@ -27,6 +27,13 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
   },
 };
 
