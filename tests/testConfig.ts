816c1c829546d988b400a6070e8e85356c46baaf
abilevych <anton@matterlabs.dev>
2023-08-28 16:00:58 +0300

    chore: updated config file

--- tests/testConfig.ts
+++ tests/testConfig.ts
@@ -2,14 +2,8 @@ import { Wallets } from "./testData";
 
 export const localConfig = {
   gasLimit: { gasLimit: 8000000 },
-  l2GasLimit: 8000000,
   L1Network: "http://127.0.0.1:8545",
   L2Network: "http://127.0.0.1:8011",
   chainId: 260,
   privateKey: Wallets.richWalletPrivateKey,
-  extendedTimeout: 1200 * 1000,
-  standardTimeout: 60 * 1000,
-  extendedPause: 20 * 1000,
-  standardPause: 5 * 1000,
-  minimalPause: 1 * 1000,
 };
