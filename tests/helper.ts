09e47e7ce7507cd74f9cdbd81f281991e8286f45
abilevych <105042995+abilevych@users.noreply.github.com>
2023-09-05 16:43:20 +0300

    Update tests/helper.ts
    Co-authored-by: Dennis <10233439+idea404@users.noreply.github.com>

--- tests/helper.ts
+++ tests/helper.ts
@@ -1,17 +1,13 @@
-let result: any;
-
 export class Helper {
   /**
    * Checks the validity of an Ethereum address (wallet/contract).
    * @param {string} value - The input string to validate.
    * @returns {boolean} - Returns `true` if the input matches the expected Ethereum value format, and `false` otherwise.
    */
-  async isValidEthFormat(value: any) {
+  async isValidEthFormat(value: string): boolean {
     if (typeof value !== "string" || !value.match(/^(0x)?[0-9a-fA-F]{40}$/)) {
-      result = false;
-    } else {
-      result = true;
+      return false;
     }
-    return result;
+    return true;
   }
 }
