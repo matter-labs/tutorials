31977a1faab26469106e8ee9fe799f094a9dc1ac
Dennis <10233439+idea404@users.noreply.github.com>
2023-10-31 16:37:52 +0100

    feat: custom-aa changes

--- custom-aa/test/utils/utils.ts
+++ custom-aa/test/utils/utils.ts
@@ -138,6 +138,7 @@ export class Utils {
       await tx.wait(1);
       this.txHash = tx.hash;
     } catch (e) {
+      console.log(e);
       return e;
     }
     // Getting the address of the deployed contract account
