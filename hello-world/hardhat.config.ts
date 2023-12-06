020a3e4f31165c3a9001f7d70fc5fd503e5a04f0
amelnytskyi <a.melnytskyi@matterlabs.dev>
2023-12-06 18:18:38 +0200

    fix(sepolia): fixing hello-world tutorial

--- hello-world/hardhat.config.ts
+++ hello-world/hardhat.config.ts
@@ -2,21 +2,29 @@ import { HardhatUserConfig } from "hardhat/config";
 
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-node";
 
 import "@matterlabs/hardhat-zksync-verify";
+
+const zkSyncTestnet = {
+  url: "https://sepolia.era.zksync.dev",
+  ethNetwork: "sepolia",
+  zksync: true,
+  // contract verification endpoint
+  verifyURL:
+    "https://explorer.sepolia.era.zksync.dev/contract_verification",
+};
 
 const config: HardhatUserConfig = {
   zksolc: {
     version: "latest",
-    settings: {
-      isSystem: true,
-    },
+    settings: {},
   },
+  defaultNetwork: "zkSyncTestnet",
   networks: {
     hardhat: {
-      zksync: true,
+      zksync: false,
     },
+    zkSyncTestnet,
   },
   solidity: {
     version: "0.8.17",
