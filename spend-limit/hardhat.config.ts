d840ec96a57342e91ce1039c01450585f9bddc1f
albionahoti <albionahh@outlook.com>
2023-12-08 00:31:14 +0000

    fix(hardhat-config): removed other networks

--- spend-limit/hardhat.config.ts
+++ spend-limit/hardhat.config.ts
@@ -14,28 +14,6 @@ const config: HardhatUserConfig = {
       zksync: true,
       verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
     },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-    },
-    zkSyncGoerliTestnet: { // deprecated network
-      url: "https://testnet.era.zksync.dev",
-      ethNetwork: "goerli",
-      zksync: true,
-      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
-    },
-    dockerizedNode: {
-      url: "http://localhost:3050",
-      ethNetwork: "http://localhost:8545",
-      zksync: true,
-    },
-    inMemoryNode: {
-      url: "http://127.0.0.1:8011",
-      ethNetwork: "", // in-memory node doesn't support eth node; removing this line will cause an error
-      zksync: true,
-    },
     hardhat: {
       zksync: true,
     },
