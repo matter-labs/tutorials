cadf02640215433a998053e1bd0047f9940bbe2c
albionahoti <albionahh@outlook.com>
2023-12-12 23:10:28 +0000

    fix(hardhat): update hardhat config for spending limit

--- spend-limit/hardhat.config.ts
+++ spend-limit/hardhat.config.ts
@@ -1,33 +1,27 @@
-import "@matterlabs/hardhat-zksync-node";
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-verify";
-
 import { HardhatUserConfig } from "hardhat/config";
 
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+import "@matterlabs/hardhat-zksync-verify";
+
 const config: HardhatUserConfig = {
+  zksolc: {
+    version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
+    settings: {
+      isSystem: true, // make sure to include this line
+    },
+  },
   defaultNetwork: "zkSyncTestnet",
   networks: {
     zkSyncTestnet: {
       url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
+      ethNetwork: "sepolia", // Can also be the RPC URL of the network (e.g. `https://sepolia.infura.io/v3/<API_KEY>`)
       zksync: true,
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
-    },
-    hardhat: {
-      zksync: true,
-    },
-  },
-  zksolc: {
-    version: "latest",
-    settings: {
-      isSystem: true
-      // find all available options in the official documentation
-      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
     },
   },
   solidity: {
-    version: "0.8.20",
+    version: "0.8.17",
   },
 };
 
