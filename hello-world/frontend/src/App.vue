70d5d18db1367dd423b9e9ecaf8bd43e3180f90b
Antonio <tonioufa@gmail.com>
2024-01-29 12:30:02 +0000

    feat: upgrades to vue3 ethers v6

--- hello-world/frontend/src/App.vue
+++ hello-world/frontend/src/App.vue
@@ -1,9 +1,201 @@
+<script setup lang="ts" >
+import { ref, onMounted } from 'vue'
+// TODO: import ethers and zksync-ethers
+
+const ETH_ADDRESS = "0x0000000000000000000000000000000000000000";
+import * as allowedTokens from "./eth.json"; // change to "./erc20.json" to use ERC20 tokens
+
+const GREETER_CONTRACT_ADDRESS = ""; // TODO: insert the Greeter contract address here
+import * as GREETER_CONTRACT_ABI from './abi.json' // TODO: Complete and import the ABI
+
+// reactive references
+const correctNetwork = ref(false)
+const tokens = ref(allowedTokens.default);
+const newGreeting = ref("");
+const greeting = ref("")
+const mainLoading = ref(true)
+const retreivingFee = ref(false)
+const retreivingBalance= ref(false)
+const currentBalance = ref("")
+const currentFee = ref("")
+const selectedToken = ref(null)
+const  selectedTokenAddress = ref(null)
+// txStatus is a reactive variable that tracks the status of the transaction
+// 0 stands for no status, i.e no tx has been sent
+// 1 stands for tx is beeing submitted to the operator
+// 2 stands for tx awaiting commit
+// 3 stands for updating the balance and greeting on the page
+const txStatus = ref(0)
+
+let provider:Provider = null
+let signer:Wallet = null
+let contract:Contract = null
+
+// Lifecycle hook
+onMounted(async () => {
+  const network = await window.ethereum.request({ method: "net_version" });
+  if(+network == 300){
+    correctNetwork.value = true;
+  }
+});
+
+ const initializeProviderAndSigner= async ()=>{
+    // TODO: initialize provider and signer based on `window.ethereum`
+    provider = new Provider('https://sepolia.era.zksync.dev');
+    // Note that we still need to get the Metamask signer
+    signer = await (new BrowserProvider(window.ethereum)).getSigner();
+    contract = new Contract(
+        GREETER_CONTRACT_ADDRESS,
+        GREETER_CONTRACT_ABI.default,
+        signer
+    );
+
+  }
+
+  const getGreeting = async ()=> {
+    // TODO: return the current greeting
+     return "";
+  }
+
+  const getFee = async() => {
+    // TODO: return formatted fee
+    return "";
+  }
+
+  const getBalance = async()=> {
+    // TODO: Return formatted balance
+    return "";
+  }
+  const getOverrides = async() => {
+    if (selectedToken.value.l2Address != ETH_ADDRESS) {
+      // TODO: Return data for the paymaster
+    }
+
+    return {};
+  }
+  const changeGreeting = async()=> {
+      txStatus.value = 1;
+      try {
+        // TODO: Submit the transaction
+        txStatus.value = 2;
+
+        // TODO: Wait for transaction compilation
+        txStatus.value = 3;
+
+        // Update greeting
+        greeting.value = await getGreeting();
+
+        retreivingFee.value = true;
+        retreivingBalance.value = true;
+        // Update balance and fee
+        currentBalance.value = await getBalance();
+        currentFee.value = await getFee();
+      } catch (e) {
+        console.error(e);
+        alert(e);
+      }
+
+      txStatus.value = 0;
+      retreivingFee.value = false;
+      retreivingBalance.value = false;
+      newGreeting.value = "";
+
+     
+    }
+
+    const updateFee = async()=>{
+      retreivingFee.value = true;
+      getFee()
+        .then((fee) => {
+          currentFee.value = fee;
+        })
+        .catch((e) => console.log(e))
+        .finally(() => {
+          retreivingFee.value = false;
+        });
+    }
+    const updateBalance = async()=>{
+      retreivingBalance.value = true;
+      getBalance()
+        .then((balance) => {
+          currentBalance.value = balance;
+        })
+        .catch((e) => console.log(e))
+        .finally(() => {
+          retreivingBalance.value = false;
+        });
+    }
+    const changeToken = async()=>{
+      retreivingFee.value = true;
+      retreivingBalance.value = true;
+      
+      const tokenAddress = tokens.value.filter(
+        (t) => t.address == selectedTokenAddress.value,
+      )[0];
+      selectedToken.value = {
+        l2Address: tokenAddress.address,
+        decimals: tokenAddress.decimals,
+        symbol: tokenAddress.symbol,
+      };
+      try{
+          updateFee();
+          updateBalance();
+        }
+      catch(e){
+        console.log(e)
+      }
+      finally{
+        retreivingFee.value = false;
+        retreivingBalance.value = false;
+      }
+    }
+    const loadMainScreen = async()=> {
+      await initializeProviderAndSigner();
+
+      if (!provider || !signer) {
+        alert("Follow the tutorial to learn how to connect to Metamask!");
+        return;
+      }
+
+      greeting.value = await getGreeting().catch((e) => console.log(e));
+        
+      mainLoading.value = false;
+      
+    }
+    const addZkSyncSepolia = async () =>{
+      // add zkSync testnet to Metamask
+      await window.ethereum.request({ method: "wallet_addEthereumChain",params: [
+        {
+          chainId: '0x12C',
+          chainName: 'zkSync Sepolia testnet',
+          rpcUrls: ['https://sepolia.era.zksync.dev'],
+          blockExplorerUrls: ['https://sepolia.explorer.zksync.io/'],
+          nativeCurrency: {
+            name: 'ETH',
+            symbol: 'ETH', 
+            decimals: 18,
+          },
+        }
+      ]})
+      window.location.reload();
+    }
+    const connectMetamask = async ()=> {
+      await window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .catch((e) => console.log(e));
+
+        loadMainScreen()
+    }
+
+</script>
+
 <template>
-  <div id="app" v-if="!mainLoading">
+   <div id="app" v-if="!mainLoading">
     <h1>Greeter says: {{ greeting }} 👋</h1>
-    <div>
+    <div class="title">
       This a simple dApp, which can choose fee token and interact with the
-      `Greeter` smart contract.
+      `Greeter` smart contract. 
+      <p>The contract is deployed on the zkSync testnet on <a :href="`https://sepolia.explorer.zksync.io/address/${GREETER_CONTRACT_ADDRESS}`" target="_blank">{{ GREETER_CONTRACT_ADDRESS }}</a></p>
     </div>
     <div class="main-box">
       <div>
@@ -56,209 +248,43 @@
   <div id="app" v-else>
     <div class="start-screen">
       <h1>Welcome to Greeter dApp!</h1>
-      <button v-on:click="connectMetamask">Connect Metamask</button>
+      <button v-if="correctNetwork" v-on:click="connectMetamask">Connect Metamask</button>
+      <button v-else v-on:click="addZkSyncSepolia">Switch to zkSync Sepolia</button>
+
     </div>
   </div>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<script>
-// eslint-disable-next-line
-const GREETER_CONTRACT_ADDRESS = ""; // TODO: Add smart contract address
-// eslint-disable-next-line
-const GREETER_CONTRACT_ABI = []; // TODO: Complete and import the ABI
-
-const ETH_L1_ADDRESS = "0x0000000000000000000000000000000000000000";
-const allowedTokens = require("./eth.json");
-
-export default {
-  name: "App",
-  data() {
-    return {
-      newGreeting: "",
-      greeting: "unknown",
-      tokens: allowedTokens,
-      selectedToken: null,
-      selectedTokenAddress: "",
-      mainLoading: true,
-      provider: null,
-      signer: null,
-      contract: null,
-      canSubmit: true,
-      // 0 stands for no status, i.e no tx has been sent
-      // 1 stands for tx is beeing submitted to the operator
-      // 2 stands for tx awaiting commit
-      // 3 stands for updating the balance and greeting on the page
-      txStatus: 0,
-      retreivingFee: false,
-      retreivingBalance: false,
-
-      currentBalance: "",
-      currentFee: "",
-    };
-  },
-  methods: {
-    initializeProviderAndSigner() {
-      // TODO: initialize provider and signer based on `window.ethereum`
-    },
-    async getGreeting() {
-      // TODO: return the current greeting
-      return "";
-    },
-    async getFee() {
-      // TOOD: return formatted fee
-      return "";
-    },
-    async getBalance() {
-      // Return formatted balance
-      return "";
-    },
-    async getOverrides() {
-      if (this.selectedToken.l1Address != ETH_L1_ADDRESS) {
-        // TODO: Return data for the paymaster
-      }
-
-      return {};
-    },
-    async changeGreeting() {
-      this.txStatus = 1;
-      try {
-        // TODO: Submit the transaction
-        this.txStatus = 2;
-
-        // TODO: Wait for transaction compilation
-        this.txStatus = 3;
-
-        // Update greeting
-        this.greeting = await this.getGreeting();
-
-        this.retreivingFee = true;
-        this.retreivingBalance = true;
-        // Update balance and fee
-        this.currentBalance = await this.getBalance();
-        this.currentFee = await this.getFee();
-      } catch (e) {
-        alert(JSON.stringify(e));
-      }
-
-      this.txStatus = 0;
-      this.retreivingFee = false;
-      this.retreivingBalance = false;
-    },
-
-    updateFee() {
-      this.retreivingFee = true;
-      this.getFee()
-        .then((fee) => {
-          this.currentFee = fee;
-        })
-        .catch((e) => console.log(e))
-        .finally(() => {
-          this.retreivingFee = false;
-        });
-    },
-    updateBalance() {
-      this.retreivingBalance = true;
-      this.getBalance()
-        .then((balance) => {
-          this.currentBalance = balance;
-        })
-        .catch((e) => console.log(e))
-        .finally(() => {
-          this.retreivingBalance = false;
-        });
-    },
-    changeToken() {
-      this.retreivingFee = true;
-      this.retreivingBalance = true;
-      const l1Token = this.tokens.filter(
-        (t) => t.address == this.selectedTokenAddress,
-      )[0];
-      this.provider
-        .l2TokenAddress(l1Token.address)
-        .then((l2Address) => {
-          this.selectedToken = {
-            l1Address: l1Token.address,
-            l2Address: l2Address,
-            decimals: l1Token.decimals,
-            symbol: l1Token.symbol,
-          };
-          this.updateFee();
-          this.updateBalance();
-        })
-        .catch((e) => console.log(e))
-        .finally(() => {
-          this.retreivingFee = false;
-          this.retreivingBalance = false;
-        });
-    },
-    loadMainScreen() {
-      this.initializeProviderAndSigner();
-
-      if (!this.provider || !this.signer) {
-        alert("Follow the tutorial to learn how to connect to Metamask!");
-        return;
-      }
-
-      this.getGreeting().then((greeting) => {
-        this.greeting = greeting;
-        this.mainLoading = false;
-      });
-    },
-    connectMetamask() {
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then(() => {
-          if (+window.ethereum.networkVersion == 280) {
-            this.loadMainScreen();
-          } else {
-            alert("Please switch network to zkSync!");
-          }
-        })
-        .catch((e) => console.log(e));
-    },
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 30px;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-#app ul {
-  display: inline-block;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-.main-box {
-  text-align: left;
-  width: 400px;
-
-  margin: auto;
-  margin-top: 40px;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
-
-.greeting-input {
-  margin-top: 20px;
+input, select{
+  padding: 8px 3px;
+  margin: 0 5px;
 }
-
-.change-button {
-  margin-left: 20px;
+button{
+  margin: 0 5px;
 }
-
-.start-screen {
-  margin-top: 100px;
-}
-
-.balance {
-  margin-top: 10px;
-}
-
-.refresh-button {
-  margin-left: 15px;
+.title, .main-box, .greeting-input, .balance{
+  margin: 10px;
 }
 </style>
