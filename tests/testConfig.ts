9314bbae28b2e02e93eac71b99d546bbf5ff0df5
Antonio <tonioufa@gmail.com>
2024-07-30 15:54:44 +0100

    fix: increases default gas limit for tests

--- tests/testConfig.ts
+++ tests/testConfig.ts
@@ -1,7 +1,7 @@
 import { Wallets } from "./testData";
 
 export const localConfig = {
-  gasLimit: { gasLimit: 8000000 },
+  gasLimit: { gasLimit: 90000000 },
   L1Network: "http://127.0.0.1:8545",
   L2Network: "http://127.0.0.1:8011",
   chainId: 260,
